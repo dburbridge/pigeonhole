dd539be95d55c250bd2c83f8e669b7bb5efe90c6
dburbridge <daniel.burbridge@publishingtechnology.com>
2016-10-28 16:47:28 +0100

    add cron

--- docker/init.sh
+++ docker/init.sh
@@ -8,6 +8,9 @@ influx -execute 'CREATE DATABASE pigeonhole'
 influx -execute "CREATE USER pigeonhole WITH PASSWORD 'pigeonhole'" -database="pigeonhole"
 influx -execute "CREATE USER pigeonhole_rw WITH PASSWORD 'SD5hsfdg8'" -database="pigeonhole"
 
+/etc/init.d/cron start
+/etc/init.d/anacron start
+
 bundle exec shotgun --host 0.0.0.0
 
 tail -f /dev/null
